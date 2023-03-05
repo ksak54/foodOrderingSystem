98d3ff581900a049a19808312601801f2d2bba17
ksak54 <kumarsaksham54@gmail.com>
2023-03-05 21:43:52 +0530

    reverting test changes

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class OrderTrackCommandHandler {
 
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery) {
-        return  null;
+        return null;
 
     }
 }
